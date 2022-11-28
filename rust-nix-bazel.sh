13b7a6add6f2f4b0276b71c31c0f1254ddf3d7dd
Joe Neeman <joeneeman@gmail.com>
2022-11-28 10:05:39 -0600

    Little shell script.

---
+++ rust-nix-bazel.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd /home/jneeman/tweag/rust-nix-bazel
+
+# cargo build > /dev/null 2>&1
+
+# export RUST_BACKTRACE=1
+
+./target/debug/rust-nix-bazel 2> /tmp/rust-nix-bazel.log
