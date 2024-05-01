3a2d14f9405b0acd09c863f75b96b8aba8844033
Joe Neeman <joe.neeman@tweag.io>
2024-05-01 16:03:31 -0500

    Add NarHash::from_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -207,6 +207,37 @@ pub struct NarHash {
     pub data: ByteBuf,
 }
 
+impl NarHash {
+    pub fn from_bytes(bytes: &[u8]) -> NarHash {
+        const BASE32_CHARS: &[u8] = b"0123456789abcdfghijklmnpqrsvwxyz";
+
+        let len = (bytes.len() * 8 - 1) / 5 + 1;
+
+        let data = (0..len)
+            .rev()
+            .map(|n| {
+                let b: usize = n * 5;
+                let i: usize = b / 8;
+                let j: usize = b % 8;
+                // bits from the lower byte
+                let v1 = bytes[i].checked_shr(j as u32).unwrap_or(0);
+                // bits from the upper byte
+                let v2 = if i >= bytes.len() - 1 {
+                    0
+                } else {
+                    bytes[i + 1].checked_shl(8 - j as u32).unwrap_or(0)
+                };
+                let v: usize = (v1 | v2) as usize;
+                BASE32_CHARS[v % BASE32_CHARS.len()]
+            })
+            .collect::<Vec<_>>();
+
+        NarHash {
+            data: ByteBuf::from(data),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct ValidPathInfoWithPath {
     pub path: StorePath,
